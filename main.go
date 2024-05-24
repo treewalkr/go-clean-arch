11248d5df08597099f16e507610d5795df5d2f79
treewalkr <suthep@pinroj.com>
2024-05-24 18:36:03 +0700

    Integrate application and domain layer

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"clean-arch/application"
+	"clean-arch/domain"
 	"log"
+	"strconv"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -10,9 +13,39 @@ func main() {
 	// Initialize
 	app := fiber.New()
 
-	// Define a route
+	// Initialize application layer
+	userService := application.NewUserService()
+
+	// Define routes
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, Fiber!")
+	})
+
+	app.Get("/users/:id", func(c *fiber.Ctx) error {
+		id, err := strconv.Atoi(c.Params("id"))
+		if err != nil {
+			return c.Status(fiber.StatusBadRequest).SendString("Invalid user ID")
+		}
+
+		user, err := userService.GetUser(id)
+		if err != nil {
+			return c.Status(fiber.StatusNotFound).SendString("User not found")
+		}
+
+		return c.JSON(user)
+	})
+
+	app.Post("/users", func(c *fiber.Ctx) error {
+		var user domain.User
+		if err := c.BodyParser(&user); err != nil {
+			return c.Status(fiber.StatusBadRequest).SendString("Invalid request payload")
+		}
+
+		if err := userService.CreateUser(&user); err != nil {
+			return c.Status(fiber.StatusInternalServerError).SendString("Could not create user")
+		}
+
+		return c.Status(fiber.StatusCreated).JSON(user)
 	})
 
 	// Start the server
